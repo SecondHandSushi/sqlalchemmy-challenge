300bf39b6ff201c6aa21fa2200ede20314ff7d1d
SecondHandSushi <98720483+SecondHandSushi@users.noreply.github.com>
2023-12-12 23:57:37 -0600

    Submittal

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sqlalchemy-challenge
+Matthew Idle 
+Univeresity of Minnesota Data Analytics and Visulaization Bootcamp
+
+App.py
+This python app uses the sqlalchemy ORM to interface with a sqlite database, then uses Flask to create an API to interface with that sqlite database. 
+
+Basic operation:
+Import dependencies, automap bases, reflect, and save references to both tables, measurement and stations. Landing page was created to show the endpoints.
+All are static routes, with the exception of the last two, which are dynamic. The dynamic routes are accessed by entereing dates in the date range and format provided. If the date entered is outside of the given range, or the start date is after the end date, then a message is displayed prompting the user to enter a new date(s). The session is then created, and the tables are queried. A list is used to make things easier to follow when quering more than three variables. The results are then unpacked into dictionaries and jsonified and returned.
+
+
+references:
+1)Hall, Cyan. “Sqlalchemy Cheatsheet.” CyanHall.Com, 2023, www.cyanhall.com/cheatsheet/22.SQLAlchemy-cheatsheet/. 
+2)Sheet, Py. “Sqlalchemy - Pysheeet.” SQLAlchemy - Pysheeet, www.pythonsheets.com/notes/python-sqlalchemy.html. Accessed 12 Dec. 2023.
+3)Petrou, Theodore. Panda’s Cookbook: Recipes for Scientific Computing, Time Series Analysis and Data Visualization Using Python. first ed., Packt Publishing, 2017. 
--- app.py
+++ app.py
@@ -31,21 +31,21 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 @app.route("/")
-def home():
+def home():         #landing page that shows route paths
     return(
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/[start date (yyyy-mm-dd)]<br/> "
-        f"/api/v1.0/[start date (yyyy-mm-dd)]/[end date (yyyy-mm-dd)]<br/>"
+        f"/api/v1.0/[start date (yyyy-mm-dd)] Date must be between 2010-01-01 and 2017-08-23<br/> " #use details to show user how to query the right data
+        f"/api/v1.0/[start date (yyyy-mm-dd)]/[end date (yyyy-mm-dd)] Date must be between 2010-01-01 and 2017-08-23<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
     
-    sel = [measurement.date, measurement.prcp]
+    sel = [measurement.date, measurement.prcp]      
     result = session.query(*sel).\
         filter(measurement.date >= '2016-08-23').\
         all()
@@ -54,10 +54,10 @@ def precipitation():
     
     precipitation = []
     for date,prcp  in result:
-        precip_dict = {}
-        precip_dict["date"] = date
-        precip_dict["prcp"] = prcp              
-        precipitation.append(precip_dict)
+        result_data= {}
+        result_data["date"] = date
+        result_data["prcp"] = prcp              
+        precipitation.append(result_data)
         
     return jsonify(precipitation)
 
@@ -68,21 +68,22 @@ def stations():
     sel = [station.station,station.name,station.latitude,station.longitude,station.elevation]
     result = session.query(*sel).all()
     session.close() 
-    
+    #Create a dictionary from the row data and append to a list
     stations_data = []
     for stat,name,latitude,longitude,elevation  in result:
-        station_info = {}
-        station_info["station"] = stat
-        station_info["name"] = name
-        station_info["latitude"] = latitude
-        station_info["longitude"] = longitude
-        station_info["elevation"] = elevation            
-        stations_data.append(station_info)
+        result_data = {}
+        result_data["station"] = stat
+        result_data["name"] = name
+        result_data["latitude"] = latitude
+        result_data["longitude"] = longitude
+        result_data["elevation"] = elevation            
+        stations_data.append(result_data)
     
     return jsonify(stations_data)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    
     session = Session(engine)
     result = session.query(measurement.date, measurement.tobs).\
         filter(measurement.station == 'USC00519281').\
@@ -91,48 +92,61 @@ def tobs():
      
     tmp_obs = []
     for date,tobs  in result:
-        tobs_dict = {}
-        tobs_dict["date"] = date
-        tobs_dict["tobs"] = tobs             
-        tmp_obs.append(tobs_dict)
+        result_data = {}
+        result_data["date"] = date
+        result_data["tobs"] = tobs             
+        tmp_obs.append(result_data)
     
     return jsonify(tmp_obs)
 
 @app.route("/api/v1.0/<start>")
 def dynamic_start(start):
-    session = Session(engine)
+    
+    if start > "2017-08-23" or  start < "2010-01-01":
+        return("HEY YOU! The date entered is out of range! The date must be between 2010-01-01 and 2017-08-23")
+    else: 
+        session = Session(engine)
+        sel = [func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)]
+        result = session.query(*sel).\
+            filter(measurement.date >= start).all()          
+        session.close()
 
-    sel = [func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)]
-    result = session.query(func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)).\
-        filter(measurement.date >= start).all()          
-    session.close()
-    tobs_ds = []
-    for tobs_min,tobs_max,tobs_avg  in result:
-        result_ds = {}
-        result_ds["tobs_min"] = tobs_min
-        result_ds["tobs_max"] = tobs_max
-        result_ds["tobs_avg"] = tobs_avg            
-        tobs_ds.append(result_ds)
+        tobs_ds = []
+        for tobs_min,tobs_max,tobs_avg  in result:
+            result_data = {}
+            result_data["tobs_min"] = tobs_min
+            result_data["tobs_max"] = tobs_max
+            result_data["tobs_avg"] = tobs_avg            
+            tobs_ds.append(result_data)
         
     return jsonify(tobs_ds)
+
 @app.route("/api/v1.0/<start>/<end>")
 def dynamic_start_end(start,end):
-    session = Session(engine)
-    sel = [func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)]
-    result = session.query(*sel).\
-        filter(measurement.station == 'USC00519281').\
-        filter(measurement.date >= start).\
-        filter(measurement.date <= end).all()        
-    session.close()
-    tobs_dse = []
-    for tobs_min,tobs_max,tobs_avg  in result:
-        result_dse = {}
-        result_dse["tobs_min"] = tobs_min
-        result_dse["tobs_max"] = tobs_max
-        result_dse["tobs_avg"] = tobs_avg            
-        tobs_dse.append(result_dse)
+    if start > "2017-08-23" or  start < "2010-01-01":
+        return("HEY YOU! The start date entered is out of range! The date must be between 2010-01-01 and 2017-08-23")
+    elif end > "2017-08-23" or  end < "2010-01-01":
+        return("HEY BUDDY! The end date entered is out of range! The date must be between 2010-01-01 and 2017-08-23")
+    elif start > end:
+        return("HEY PAL! The start date cannot be later than the end date")
+    else:
+        session = Session(engine)
+        sel = [func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs)]
+        result = session.query(*sel).\
+            filter(measurement.station == 'USC00519281').\
+            filter(measurement.date >= start).\
+            filter(measurement.date <= end).all()        
+        session.close()
         
-    return jsonify(tobs_dse)
-
+        tobs_dse = []
+        for tobs_min,tobs_max,tobs_avg  in result:
+            result_data = {}
+            result_data["tobs_min"] = tobs_min
+            result_data["tobs_max"] = tobs_max
+            result_data["tobs_avg"] = tobs_avg            
+            tobs_dse.append(result_data)
+            
+        return jsonify(tobs_dse)
+    
 if __name__ == "__main__":
     app.run(debug=True)
